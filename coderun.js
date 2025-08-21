226a5d80b54dcedf504731dee915b1ddd68cb164
JoseSilvaIT <josesilvait2025@gmail.com>
2025-08-21 11:05:21 -0500

    ActualizacionPaginaCalletana

---
+++ coderun.js
@@ -0,0 +1,14 @@
+/* JAVASCRIPT
+    JavaScritp es un Lenguage Divertido;
+    Dijo Nadie Nunca 
+*/
+
+// Este codigo es para ver si un Numero es Par en una cuenta de numeros
+var mondongo = 0;
+var CantidadNum = 30
+while (mondongo <= CantidadNum) {
+    if (mondongo % 2 == 0) {
+        console.log(mondongo + " es par");
+    }
+    mondongo++
+}
